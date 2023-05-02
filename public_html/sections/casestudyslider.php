8c6aae8c9529a339364a04499d9ec9409234a741
praty89 <pratyushsakhlecha@gmail.com>
2023-05-03 03:59:34 +0530

    rel="canonical" added to links

--- public_html/sections/casestudyslider.php
+++ public_html/sections/casestudyslider.php
@@ -14,7 +14,7 @@
         <div class="post-img"><img src="assets/img/casestudy/cs1.jpg" class="img-fluid" alt=""></div>
         <span class="post-date">Tue, September 15</span>
         <h3 class="post-title">ARSS INFRASTRUCTURE PROJECTS LIMITED SAVES $130,000 (approx. ₹1 Cr) PER MONTH WITH FUEL-LEVEL SENSORS</h3>
-        <a href="?page=CaseStudy-single-1" class="readmore stretched-link mt-auto"><span>Read More</span><i class="bi bi-arrow-right"></i></a>
+        <a rel="canonical" href="?page=CaseStudy-single-1" class="readmore stretched-link mt-auto"><span>Read More</span><i class="bi bi-arrow-right"></i></a>
       </div>
     </div>
 
@@ -23,7 +23,7 @@
         <div class="post-img"><img src="assets/img/casestudy/cs2.jpg" class="img-fluid" alt=""></div>
         <span class="post-date">Fri, August 28</span>
         <h3 class="post-title">DHARA LOGISTICS INCREASES PROFITABILITY BY 12%</h3>
-        <a href="?page=CaseStudy-single-2" class="readmore stretched-link mt-auto"><span>Read More</span><i class="bi bi-arrow-right"></i></a>
+        <a rel="canonical" href="?page=CaseStudy-single-2" class="readmore stretched-link mt-auto"><span>Read More</span><i class="bi bi-arrow-right"></i></a>
       </div>
     </div>
 
@@ -32,7 +32,7 @@
         <div class="post-img"><img src="assets/img/casestudy/cs3.jpg" class="img-fluid" alt=""></div>
         <span class="post-date">Mon, July 11</span>
         <h3 class="post-title">SR Ferro Alloys caught 400 liters of Diesel theft in the first month</h3>
-        <a href="?page=CaseStudy-single-3" class="readmore stretched-link mt-auto"><span>Read More</span><i class="bi bi-arrow-right"></i></a>
+        <a rel="canonical" href="?page=CaseStudy-single-3" class="readmore stretched-link mt-auto"><span>Read More</span><i class="bi bi-arrow-right"></i></a>
       </div>
     </div>
 
