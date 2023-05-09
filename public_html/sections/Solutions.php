27653446fe8d5c103afadf11cf8b013ea216a5b2
praty89 <pratyushsakhlecha@gmail.com>
2023-05-10 03:07:11 +0530

    Fixed placements of txt and img in solutions sec

--- public_html/sections/Solutions.php
+++ public_html/sections/Solutions.php
@@ -8,13 +8,22 @@
         
         <div class="container" data-aos="fade-up" style="margin-right: 1px;">
             <div class="row">
-              <div class="col-lg-6 d-flex flex-column justify-content-center order-sm-2 order-md-2 order-lg-1">
-                <h2 data-aos="fade-up">Maximize Your Fuel Savings with OFeX's Fleet and Fuel Optimisation Solution                
+              
+              <div class="col-lg-6 hero-img order-lg-2" data-aos="zoom-out" data-aos-delay="200">
+                <img src="assets/img/fleet-hero-4.png" class="img-fluid" alt="">
+              </div> 
+
+              <div class="col-lg-6 d-flex flex-column justify-content-center order-lg-1">
+                
+                <h2 data-aos="fade-up">
+                  Maximize Your Fuel Savings with OFeX's Fleet and Fuel Optimisation Solution                
                 </h2>
-                <h6 data-aos="fade-up" data-aos-delay="400"><br>
-                  At OFeX, we excel in engineering <b>Telematics Solutions for Fleet Management</b>, with <b>cutting-edge hardware</b> for diverse industries. Our expertise lies in identifying crucial data for <b>fuel consumption management</b> and empowering clients with their techniques and operations for industrial efficiency.<br><br>Enhance efficiency, safety, and cost savings by digitizing your end-to-end logistics and fleet management plan with OFeX’s versatile solutions.
+
+                <h6 data-aos="fade-up" data-aos-delay="400">
+                  <br>At OFeX, we excel in engineering <b>Telematics Solutions for Fleet Management</b>, with <b>cutting-edge hardware</b> for diverse industries. Our expertise lies in identifying crucial data for <b>fuel consumption management</b> and empowering clients with their techniques and operations for industrial efficiency.<br><br>Enhance efficiency, safety, and cost savings by digitizing your end-to-end logistics and fleet management plan with OFeX’s versatile solutions.
                   <br><br><br>
                 </h6>
+                
                 <div class="text-center text-lg-start">
                   <a href="?page=aboutofex" class="btn-read-more d-inline-flex align-items-center justify-content-center align-self-center">
                     <span>About Us</span>
@@ -22,9 +31,7 @@
                   </a>
                 </div>
               </div>
-              <div class="col-lg-6 hero-img order-sm-1 order-md-1 order-lg-2" data-aos="zoom-out" data-aos-delay="200">
-                <img src="assets/img/fleet-hero-4.png" class="img-fluid" alt="">
-              </div>             
+           
             </div>
         </div>
 
@@ -35,12 +42,12 @@
         <div class="container" data-aos="fade-up">
           <div class="row gx-0"><!-- ======= Solutions 1 ======= -->
 
-            <div class="col-lg-6 d-flex align-items-center order-sm-1 order-md-1 order-lg-1 " data-aos="zoom-out" data-aos-delay="200">
+            <div class="col-lg-6 d-flex align-items-center order-lg-1 " data-aos="zoom-out" data-aos-delay="200">
               <img style="max-width: 60%;" src="assets/img/fleetm.png" class="img-fluid mx-auto" alt="">
             </div>
             
 
-            <div class="col-lg-6 d-flex flex-column justify-content-center order-sm-2 order-md-2 order-lg-2 " data-aos="fade-up" data-aos-delay="200">
+            <div class="col-lg-6 d-flex flex-column justify-content-center order-lg-2 " data-aos="fade-up" data-aos-delay="200">
               <div class="content">
                 <h2>Fleet Management Solution</h2>
                 <h3>OFeX's advanced Fleet Management System streamlines your Fleet by - </h3>
@@ -53,7 +60,7 @@
                     </ul>
                 </p>
                 <!--div class="text-center text-lg-start">
-                  <a href="#pricing" class="btn-read-more d-inline-flex align-items-center justify-content-center align-self-center">
+                  <a href="#" class="btn-read-more d-inline-flex align-items-center justify-content-center align-self-center">
                     <span>Know More</span>
                     <i class="bi bi-arrow-right"></i>
                   </a>
@@ -64,9 +71,11 @@
           
           <div class="row gx-0"><!-- ======= Solutions 2 ======= -->
 
-            
+            <div class="col-lg-6 d-flex align-items-center order-lg-2" data-aos="zoom-out" data-aos-delay="200">
+              <img style="max-width: 60%;" src="assets/img/fuelm.gif" class="img-fluid mx-auto" alt="">
+            </div>
 
-            <div class="col-lg-6 d-flex flex-column justify-content-center order-sm-2 order-md-2 order-lg-1" data-aos="fade-up" data-aos-delay="200">
+            <div class="col-lg-6 d-flex flex-column justify-content-center order-lg-1" data-aos="fade-up" data-aos-delay="200">
               <div class="content">
                   <h2>Fuel Management Solution</h2>
                   <h3>OFeX's Capacitive Fuel Sensing Technology streamlines Fuel Management by - </h3>
@@ -87,19 +96,15 @@
                   </div-->
               </div>
             </div>
-            
-            <div class="col-lg-6 d-flex align-items-center order-sm-1 order-md-1 order-lg-2" data-aos="zoom-out" data-aos-delay="200">
-              <img style="max-width: 60%;" src="assets/img/fuelm.gif" class="img-fluid mx-auto" alt="">
-            </div>
-
+                        
           </div>
 
           <div class="row gx-0"><!-- ======= Solutions 3 ======= -->
-            <div class="col-lg-6 d-flex align-items-center order-sm-1 order-md-1 order-lg-1" data-aos="zoom-out" data-aos-delay="200">
+            <div class="col-lg-6 d-flex align-items-center order-lg-1" data-aos="zoom-out" data-aos-delay="200">
               <img style="max-width: 60%;" src="assets/img/reportm.gif" class="img-fluid mx-auto" alt="">
             </div>
 
-            <div class="col-lg-6 d-flex flex-column justify-content-center order-sm-2 order-md-2 order-lg-2" data-aos="fade-up" data-aos-delay="200">
+            <div class="col-lg-6 d-flex flex-column justify-content-center order-lg-2" data-aos="fade-up" data-aos-delay="200">
 
               <div class="content">
                   <h2>Automated Telematics Data Reports</h2>
