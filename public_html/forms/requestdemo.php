fc8cdf1fed23a3e0325da5686849f0d93b6445c5
praty89 <pratyushsakhlecha@gmail.com>
2023-05-15 23:20:18 +0530

    added book a demo gform

--- public_html/forms/requestdemo.php
+++ public_html/forms/requestdemo.php
@@ -1,19 +1,46 @@
 <?php
-  /**
-  * Requires the "PHP Email Form" library
-  * The "PHP Email Form" library is available only in the pro version of the template
-  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
-  * For more info and help: https://bootstrapmade.com/php-email-form/
-  */
 
   // Replace contact@example.com with your real receiving email address
   $receiving_email_address = 'ofexpvtltd@gmail.com';
+  
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
   } else {
     die( 'Unable to load the "PHP Email Form" Library!');
   }
+
+  // Gather form data
+  $name = $_POST['name'];
+  $email = $_POST['email'];
+  $contact = $_POST['contact'];
+  $company = $_POST['company'];
+  $fleet = $_POST['fleet'];
+  $message = $_POST['message'];
+
+  // Submit data to Google Form
+  $formData = [
+    'entry.645145372' => $name,
+    'entry.1653401476' => $email,
+    'entry.1784894739' => $contact,
+    'entry.2088655159' => $company,
+    'entry.1929734809' => $fleet,
+    'entry.577908006' => $message,
+  ];
+
+  $googleFormUrl = 'https://docs.google.com/forms/d/e/1FAIpQLSc4ANalXhRJoOm1Kj0xdbAx8rNbl57Uiz8-Z78muGqG2RdLBg/formResponse';
+
+  $options = [
+    'http' => [
+        'header' => 'Content-Type: application/x-www-form-urlencoded',
+        'method' => 'POST',
+        'content' => http_build_query($formData),
+    ],
+  ];
+
+  $context = stream_context_create($options);
+  $result = file_get_contents($googleFormUrl, false, $context);
+
 
   $contact = new PHP_Email_Form;
   $contact->ajax = true;
@@ -26,17 +53,7 @@
   $contact->from_company = $_POST['company'];
   $contact->from_fleet = $_POST['fleet'];
   $contact->subject = 'Demo Requested by ' . $_POST['name'] . ' from ' . $_POST['company'];
-
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
-  $contact->smtp = array(
-    'host' => '',
-    'username' => '',
-    'password' => '',
-    'port' => ''
-  ); */
-  
-
+ 
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
   $contact->add_message($_POST['company'], 'Company');
